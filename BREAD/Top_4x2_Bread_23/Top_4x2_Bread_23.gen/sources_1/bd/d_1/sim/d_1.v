bd0e60f09179a3b220f2943cc31c50abddec1965
Nathan Felt <felt@fas.harvard.edu>
2025-07-30 14:13:51 -0400

    project files from implemented 2nd nyquist zone

--- BREAD/Top_4x2_Bread_23/Top_4x2_Bread_23.gen/sources_1/bd/d_1/sim/d_1.v
+++ BREAD/Top_4x2_Bread_23/Top_4x2_Bread_23.gen/sources_1/bd/d_1/sim/d_1.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2023.1 (lin64) Build 3865809 Sun May  7 15:04:56 MDT 2023
-//Date        : Fri Jul 25 16:22:49 2025
+//Date        : Wed Jul 30 12:34:47 2025
 //Host        : pc0 running 64-bit Ubuntu 24.04.2 LTS
 //Command     : generate_target d_1.bd
 //Design      : d_1
